79596ae623ce068bcba95e34d0fea6605d9ed3e3
csjoey <Joelopor@udel.edu>
2019-09-12 13:52:48 -0400

    Added joeyL.py Finished basic readme/ Work distribution, mutual practices\

---
+++ joeyL.py
@@ -0,0 +1,19 @@
+# Height 50
+# Width 30
+# Margin 10
+# Start pen bottom left
+# Space 30px
+# Todo: Y & Z Print, Parser function, and example.py
+
+
+import turtle
+import math
+
+def turtlePrint(inputStr):
+    assert isinstance(inputStr, str), "Non-String Input"
+
+
+
+
+
+
