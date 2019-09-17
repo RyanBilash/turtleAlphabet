cc6e053bd2cf68364a15e33b444b4200824b408d
alexsyou <youalex@udel.edu>
2019-09-17 11:45:09 -0400

    Changed example message

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 from main import turtlePRINT,turtleSETUP
 import joeyL,alexYou,ryanBilash
 turtleSETUP()
-turtlePRINT("The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop.")
+turtlePRINT("Hi")
